e7449181c6484d6047156dfa8c06b290adeba55d
aliciacanales <canalesalicia06@gmail.com>
2023-06-06 17:11:53 -0700

    weights script

---
+++ 2019_weights.R
@@ -0,0 +1,10 @@
+df_2019 <- coho %>% 
+  filter(year == 2019) %>% 
+  select(-year)
+
+tot_pop <- sum(df_2019)
+weights_2019 <-df_2019/tot_pop
+sum(weights_2019)
+
+var_df_fcn(weights_2019, dmat)
+mu_fcn(weights_2019, pop_return_mean)
